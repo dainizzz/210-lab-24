a525f2d85d2593694ba3d7098e8b68c16b1b6100
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-10-26 12:24:44 -0700

    10 minute update

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <iomanip>
 #include <list>
+#include <limits>
 #include "Goat.h"
 using namespace std;
 
@@ -30,8 +31,7 @@ int main() {
     fin1.close();
 
     int choice = main_menu();
-    cout << choice << endl;
-
+    list<Goat> trip;
 
     return 0;
 }
@@ -44,12 +44,28 @@ int main_menu() {
     cout << "[3] List goats" << endl;
     cout << "[4] Quit" << endl;
     cout << "Choice --> ";
-    cin >> choice;
-    // TODO: check choice is int
-    while (choice < 1 || choice > 4) {
+    while (!(cin >> choice) || choice < 1 || choice > 4) {
         cout << "Invalid choice. Please enter a number 1-4 as your choice:" << endl;
         cin.clear();
-        cin >> choice;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
     }
     return choice;
+}
+
+int select_goat(list<Goat> trip) {
+
+}
+
+void delete_goat(list<Goat> &trip) {
+}
+
+void add_goat(list<Goat> &trip, string names[], string colors[]) {
+    // randomly select a name and color from main()'s arrays
+    // select a random age between 0 and max age
+}
+
+void display_trip(list<Goat> trip) {
+    // OUTPUT FORMAT:
+    // [num] name (age, color)
+    // num starts at 1
 }
